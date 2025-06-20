38ad4bfffd2ec0f223d5ecfa01850ada207ed6be
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 04:17:11 +0000

    [automatic commit] 2025-06-20 04:17:11 UTC ;; communications error to 208.67.222.222#53: timed out 1.147.97.184

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -51,6 +51,7 @@ router.get('/api/walkers/summary', async function(req, res, next) {
       FROM Users u
       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
       LEFT JOIN WalkRequests w ON r.request_id = w.request_id
+      WHERE 
       GROUP BY u.username;`
 
     );
