2fd7161a69d1583a31055922aa052101738b5b1d
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 02:54:23 +0000

    [automatic commit] 2025-06-20 02:54:23 UTC 129.127.202.44

--- part1/app.js
+++ part1/app.js
@@ -33,15 +33,15 @@ let db;
     });
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role)
         VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('olivia1', 'olivia@example.com', 'pass123', 'walker'),
-('Bwana', 'Bwana@example.com', 'coolPassword', 'owner');
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('olivia1', 'olivia@example.com', 'pass123', 'walker'),
+        ('Bwana', 'Bwana@example.com', 'coolPassword', 'owner');
       `);
     }
   } catch (err) {
