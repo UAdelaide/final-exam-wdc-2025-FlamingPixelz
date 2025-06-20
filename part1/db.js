9d725fc752edbbfeda8a2d1dea98e9ed1e249c91
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 03:12:23 +0000

    [automatic commit] 2025-06-20 03:12:23 UTC 1.147.97.184

--- part1/db.js
+++ part1/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    password: '',
+    database: 'DogWalkService'
 });
 
 module.exports = pool.promise();
