c292d0d9db02bf4db3fe99dabf6eaa2c0e73350f
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 02:53:22 +0000

    [automatic commit] 2025-06-20 02:53:22 UTC 129.127.202.44

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -55,19 +55,19 @@ CREATE TABLE WalkRatings (
 );
 
 
--- INSERT INTO Users (username, email, password_hash, role)
--- VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
--- ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
--- ('carol123', 'carol@example.com', 'hashed789', 'owner'),
--- ('olivia1', 'olivia@example.com', 'pass123', 'walker'),
--- ('Bwana', 'Bwana@example.com', 'coolPassword', 'owner');
+INSERT INTO Users (username, email, password_hash, role)
+VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('olivia1', 'olivia@example.com', 'pass123', 'walker'),
+('Bwana', 'Bwana@example.com', 'coolPassword', 'owner');
 
--- INSERT INTO Dogs (owner_id, name, size)
--- VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
--- ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
--- ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Poppy', 'large'),
--- ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Henry', 'small'),
--- ((SELECT user_id FROM Users WHERE username = 'Bwana'), 'Enji', 'medium');
+INSERT INTO Dogs (owner_id, name, size)
+VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Poppy', 'large'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Henry', 'small'),
+((SELECT user_id FROM Users WHERE username = 'Bwana'), 'Enji', 'medium');
 
 -- INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 -- VALUES ((SELECT dog_id FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Dogs.name = 'Max' AND Users.username = 'alice123'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
