39197ef7777d7fb56c0f1bc6fe981496cb614d33
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 12:54:24 +0000

    [automatic commit] 2025-06-20 12:54:24 UTC 1.147.72.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -99,7 +99,7 @@ router.post('/logout', (req, res) => {
 // Gets the dogs of the current owner that is logged in
 router.get('/yourDogs', async (req, res) => {
 
-  console.log()
+  console.log('object:' req.se )
 
   const user_id = req.session.user.user_id;
 
