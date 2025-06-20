0222e5f2a66ffafdfdee7f93a530b29616d2b9c2
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 03:41:33 +0000

    [automatic commit] 2025-06-20 03:41:33 UTC 1.147.97.184

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -76,4 +76,4 @@ CREATE TABLE WalkRatings (
 -- ((SELECT dog_id FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE name = 'Enji' AND Users.username = 'Bwana'), '2025-06-10 10:30:00', 30, 'Glenelg Beach', 'accepted'),
 -- ((SELECT dog_id FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE name = 'Henry' AND Users.username = 'alice123'), '2025-06-11 13:30:00', 45, 'Court St', 'cancelled');
 
-INSERT INTO 
+INSERT INTO WalkRatings ()
