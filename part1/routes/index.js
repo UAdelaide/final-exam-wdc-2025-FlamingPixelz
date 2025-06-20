eb5accf0658e16c033ecde47af33151058217550
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 03:57:11 +0000

    [automatic commit] 2025-06-20 03:57:11 UTC 1.147.97.184

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -50,7 +50,7 @@ router.get('/api/walkers/summary', async function(req, res, next) {
       `SELECT u.username, r.COUNT(r.rating) AS total_ratings, AVG(r.rating) AS average_rating, COUNT(w.status) AS completed_walks
       FROM WalkRatings r
       INNER JOIN Users u ON r.walker_id = u.user_id
-      INNER JOIN WalkRequests w`
+      INNER JOIN WalkRequests w ON r.request_id = w.request_id`
 
     );
 
