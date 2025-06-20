564480965bbb634d55ed03ad4f12ce48cd2aced7
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 03:16:48 +0000

    [automatic commit] 2025-06-20 03:16:48 UTC 1.147.97.184

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -12,7 +12,8 @@ router.get('/api/dogs', async function(req, res, next) {
 
   try {
     const [dogs] = await db.execute(
-      'SELECT Dogs.name, Dogs.size FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id'
+      `SELECT Dogs.name, Dogs.size FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id`
     );
     res.json(dogs);
   } catch(err) {
