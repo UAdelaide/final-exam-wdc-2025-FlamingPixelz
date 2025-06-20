acbad55953f4528ee9994252236c5588acbae359
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 07:04:14 +0000

    [automatic commit] 2025-06-20 07:04:14 UTC 1.147.97.184

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     const user = rows[0];
+
+    if (password !== )
     // Creating data gathered from user session
     if(password === rows[0].password) {
       req.session.user = {
