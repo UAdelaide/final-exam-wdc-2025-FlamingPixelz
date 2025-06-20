0281738b43823020c30a87ea12c0c659c720f835
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 07:26:34 +0000

    [automatic commit] 2025-06-20 07:26:34 UTC 1.147.97.184

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { user_id, username, email, role, password } = req.body;
 
   try {
     const [rows] = await db.query(`
