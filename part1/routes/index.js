fae49b0847965ea051d406875163b32ea41509b2
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 02:45:53 +0000

    [automatic commit] 2025-06-20 02:45:53 UTC 129.127.202.44

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 router.get('/api/dogs', function(req, res, next) {
 
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM books');
     res.json(books);
   } catch(err) {
     res.status(500).json({ error: 'Failed to fetch dogs from database' });
