57dc30697a23e50ed04b9b3d4b38f860956f5ca0
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 01:54:49 +0000

    [automatic commit] 2025-06-20 01:54:49 UTC 129.127.202.44

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -72,5 +72,5 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id))
+((SELECT user_id FROM Users WHERE username = 'carol123'), '')
 
