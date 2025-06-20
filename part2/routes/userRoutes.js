75be595442b521442c091184fd397af7a8401c9a
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 12:03:34 +0000

    [automatic commit] 2025-06-20 12:03:34 UTC 1.147.72.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -102,6 +102,8 @@ router.get('/yourDogs', async (req, res) => {
 
   try {
      const [dogs] = await db.query('SELECT name FROM Dogs WHERE owner_id = ?', [owner_id]);
+  } catch (err) {
+    res.status(500)
   }
 
 
