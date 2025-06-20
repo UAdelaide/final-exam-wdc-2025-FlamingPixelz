e7f19c90b1c371f86d2082964ef537deb7adaf0e
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 01:41:15 +0000

    [automatic commit] 2025-06-20 01:41:15 UTC 129.127.202.44

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -62,10 +62,10 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
 
 INSERT INTO Users (username, email, password_hash, role)
-VALUES ('carol123', 'alice@example.com', 'hashed123', 'owner');
+VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
 
 INSERT INTO Users (username, email, password_hash, role)
-VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
+VALUES ('Olivia123', 'alice@example.com', 'hashed123', 'owner');
 
 INSERT INTO Users (username, email, password_hash, role)
 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
