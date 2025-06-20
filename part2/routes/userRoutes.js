5f7fd0ee554aed46daea38dbd28919ef2074f1fa
Breanna Liebelt <Breannaliebelt1@gmail.com>
2025-06-20 07:06:38 +0000

    [automatic commit] 2025-06-20 07:06:38 UTC 1.147.97.184

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,9 @@ router.post('/login', async (req, res) => {
         role: rows[0].role
       };
 
-      if(user.role === 'owner') {}
+      if(user.role === 'owner') {
+        return res.redirect('/owner-dashboard.html');
+      }
 
       res.json({ message: 'Login successful', user: rows[0] });
 
